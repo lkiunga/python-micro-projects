71584564683f6e55097208c65b2c2d0e57dfd29f
lkiunga <linet.kiunga@strathmore.edu>
2023-06-03 16:04:34 +0300

    Fix the iterate-dictionary.values()

--- shoppingcart.py
+++ shoppingcart.py
@@ -11,18 +11,18 @@ for i in range(2):
 
 print(shopping_cart)
 # remove an item from the list
-item_remove = input("Item to remove from shopping list ")
-del shopping_cart[item_remove]
+#item_remove = input("Item to remove from shopping list ")
+#del shopping_cart[item_remove]
 
 # print out each item name and price
 print(shopping_cart)
 total_sum = 0
 
 # sum up the prices in a dictionary
-for price in shopping_cart:
-    
-
-total_sum = shopping_cart[item_name] + total_sum
+#Dict-name.keys() allows to iterate and return on oly the keys
+#Dict-name.values() allows us to iterate over adictionary anf return only the values
+for price in shopping_cart.values():
+    total_sum += price
 # print the total sum   
 print(total_sum)
     
